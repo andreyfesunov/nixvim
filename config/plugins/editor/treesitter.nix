b149e692fb1d05f1f44610d6887701108368a7ea
andreyfesunov <fsnv04@gmail.com>
2025-06-06 17:16:56 +0300

    fix(treesitter): disable treesitter highlight for oil windows

--- config/plugins/editor/treesitter.nix
+++ config/plugins/editor/treesitter.nix
@@ -4,10 +4,20 @@
     enable = true;
     settings = {
       indent.enable = true;
-      highlight.enable = true;
+      highlight = {
+        enable = true;
+        disable = ''
+          function()
+            local buf_name = vim.fn.expand("%")
+            if string.find(buf_name, "oil://") then
+              return true
+            end
+          end
+        '';
+      };
     };
     folding = false;
     nixvimInjections = true;
-    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars; 
   };
 }
