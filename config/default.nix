50c5e7aa53b9eedcd019aba575d4c7781557d8a2
andreyfesunov <fsnv04@gmail.com>
2025-09-10 10:28:49 +0300

    feat(snacks): remove many plugins -> snacks

--- config/default.nix
+++ config/default.nix
@@ -4,29 +4,26 @@ _: {
     ./settings.nix
     ./keymaps.nix
 
-    # CoC (replaces builtin LSP + cmp) and handles completion, formatting, pairs
+    # CoC
     ./plugins/coc/coc.nix
 
     # Editor
     ./plugins/editor/oil.nix
     ./plugins/editor/treesitter.nix
-    ./plugins/editor/indent-blankline.nix
     ./plugins/editor/lualine.nix
-    ./plugins/editor/startify.nix
-    ./plugins/editor/fzf-lua.nix
     ./plugins/editor/barbecue.nix
     ./plugins/editor/scrollview.nix
     ./plugins/editor/colorscheme.nix
     ./plugins/editor/harpoon.nix
     ./plugins/editor/precognition.nix
+    ./plugins/editor/startify.nix
+    ./plugins/editor/snacks.nix
 
     # Git
-    ./plugins/git/lazygit.nix
     ./plugins/git/gitsigns.nix
 
     # Utils
     ./plugins/utils/web-devicons.nix
     ./plugins/utils/whichkey.nix
-    ./plugins/utils/toggleterm.nix
   ];
 }
