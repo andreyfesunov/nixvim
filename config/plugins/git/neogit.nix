52972e8ec36313d1885f2ede8a07b9b8882304b0
nixos <nixos@nixos.codemasters.local>
2026-02-18 11:57:53 +0000

    feat(neogit): add keymaps for `neogit` package

--- config/plugins/git/neogit.nix
+++ config/plugins/git/neogit.nix
@@ -1,3 +1,14 @@
 {
   plugins.neogit.enable = true;
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>gn";
+      action = "<cmd>:Neogit<cr>";
+      options = {
+        desc = "Neogit";
+      };
+    }
+  ];
 }
